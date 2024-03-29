e94031897b42466d5c4b9434e7ce649ca8d21160
ashutoshsingh200203 <ashutoshsingh.tomar.2024@gmail.com>
2024-03-29 12:19:27 +0530

    states and cities ajax task added successfully

--- src/controller/jstask.js
+++ src/controller/jstask.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const conn = require('../config/connection')
 
 exports.eventTable = async (req,res) =>{
     try {
@@ -51,6 +52,35 @@ exports.showPosts = async (req,res) => {
 exports.showComments = async (req,res)=>{
     try {
         res.sendFile(path.join(__dirname,"../../public","Async_Await","personal.html"))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.statesCities = async (req,res)=>{
+    try {
+        res.sendFile(path.join(__dirname,'../../public','State_Cities','list.html'))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.showStates = async (req,res)=>{
+    try {
+        let query = `select * from states ;`
+        let result =  await conn.promise().query(query) ;
+        console.log(result)
+        res.send(result) ;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.showCities = async (req,res)=>{
+    try {
+        let cres = await conn.promise().query(`select * from cities where state_id = ${req.params.state_id}`);
+        console.log(cres)
+        res.send(cres);
     } catch (error) {
         console.log(error)
     }
--- src/routes/jstask.js
+++ src/routes/jstask.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { eventTable, kukuCube, rowColumn, sortingTask, ticTactoe, showPosts, showComments } = require('../controller/jstask');
+const { eventTable, kukuCube, rowColumn, sortingTask, ticTactoe, showPosts, showComments, statesCities, showStates, showCities } = require('../controller/jstask');
 const { isVerified } = require('../middleware/verification');
 const router = express.Router() ;
 
@@ -10,5 +10,8 @@ router.route('/sorting').get(isVerified,sortingTask)
 router.route('/tictactoe').get(isVerified,ticTactoe)
 router.route('/posts').get(isVerified,showPosts)
 router.route('/posts/:id').get(isVerified,showComments)
+router.route('/statecities').get(isVerified,statesCities)
+router.route('/states').get(isVerified,showStates)
+router.route('/cities/:state_id').get(isVerified,showCities)
 
 module.exports = router ;
